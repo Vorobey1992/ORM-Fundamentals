93b059632f1c4838b6c1da607ffd30ec620d850d
Vitali Varabuey <72112909+Vorobey1992@users.noreply.github.com>
2023-08-15 21:11:29 +0300

    add 2 ORM, devided dataaccess, changed status

--- ORM Fundamentals/Program.cs
+++ ORM Fundamentals/Program.cs
@@ -3,7 +3,8 @@ using ORM_Fundamentals.Models;
 
 using (var context = new AppDbContext())
 {
-    var dataAccess = new DataAccess(context);
+    var productRepository = new ProductRepository(context);
+    var orderRepository = new OrderRepository(context);
 
     // Пример использования методов
     var newProduct = new Product
@@ -16,23 +17,63 @@ using (var context = new AppDbContext())
         Length = 8
     };
 
-    dataAccess.CreateProduct(newProduct);
+    productRepository.CreateProduct(newProduct);
 
     var newOrder = new Order
     {
         ProductId = newProduct.Id,
-        Status = "NotStarted",
+        Status = OrderStatus.NotStarted.ToString(),
         CreatedDate = DateTime.Now,
         UpdatedDate = DateTime.Now
     };
 
-    dataAccess.CreateOrder(newOrder);
+    orderRepository.CreateOrder(newOrder);
 
-    var orders = dataAccess.GetOrdersByFilter(DateTime.Today.AddDays(-7), DateTime.Today, "NotStarted", newProduct.Id);
+    var orders = orderRepository.GetOrdersByFilter(DateTime.Today.AddDays(-7), DateTime.Today, OrderStatus.NotStarted, newProduct.Id);
     foreach (var order in orders)
     {
         Console.WriteLine($"Order ID: {order.Id}, Status: {order.Status}");
     }
 
-    dataAccess.BulkDeleteOrdersByFilter(DateTime.Today.AddDays(-7), DateTime.Today, "NotStarted", newProduct.Id);
+    orderRepository.BulkDeleteOrdersByFilter(DateTime.Today.AddDays(-7), DateTime.Today, OrderStatus.NotStarted, newProduct.Id);
 }
+
+//
+/*
+ * using (var context = new AppDbContext())
+            {
+                var productRepository = new DapperProductRepository(context);
+                var orderRepository = new DapperOrderRepository(context);
+
+                // Пример использования методов
+                var newProduct = new Product
+                {
+                    Name = "Sample Product",
+                    Description = "A sample product description",
+                    Weight = 1.5m,
+                    Height = 10,
+                    Width = 5,
+                    Length = 8
+                };
+
+                productRepository.CreateProduct(newProduct);
+
+                var newOrder = new Order
+                {
+                    ProductId = newProduct.Id,
+                    Status = OrderStatus.NotStarted,
+                    CreatedDate = DateTime.Now,
+                    UpdatedDate = DateTime.Now
+                };
+
+                orderRepository.CreateOrder(newOrder);
+
+                var orders = orderRepository.GetOrdersByFilter(DateTime.Today.AddDays(-7), DateTime.Today, OrderStatus.NotStarted, newProduct.Id);
+                foreach (var order in orders)
+                {
+                    Console.WriteLine($"Order ID: {order.Id}, Status: {order.Status}");
+                }
+
+                orderRepository.BulkDeleteOrdersByFilter(DateTime.Today.AddDays(-7), DateTime.Today, OrderStatus.NotStarted, newProduct.Id);
+            }
+*/
